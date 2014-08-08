bd358f2b085ec44e05df9e1663ade6ee14ad03e4
Antonio Nalesso <nbit001@gmail.com>
2014-08-08 21:03:39 +0100

    AN: Tested start/stop server

--- lib/optimus_prime.rb
+++ lib/optimus_prime.rb
@@ -3,6 +3,30 @@ require "optimus_prime/server"
 
 module OptimusPrime
 
+  def self.restart_server
+    self.stop_server
+    self.start_server
+  end
+
+  def self.start_server
+    if `ls ./tmp/pids`.include?("optimus_prime.pid")
+      return `echo 'Optimus is already priming :)'`
+    else
+      `thin start -p 7002 -P ./tmp/pids/optimus_prime.pid -d`
+      while true
+        break if `ls ./tmp/pids`.include?("optimus_prime.pid")
+      end
+    end
+  end
+
+  def self.stop_server
+    cmd = `thin stop -P ./tmp/pids/optimus_prime.pid`
+    while true
+      break unless `ls ./tmp/pids`.include?("optimus_prime.pid")
+    end
+    cmd
+  end
+
   class Base
 
     def prime(path_name, response="", options={})
@@ -13,8 +37,5 @@ module OptimusPrime
       ::Faraday.get("http://localhost:7002/get", { path_name: path_name }).tap { |r| yield(r) if ::Kernel.block_given? }.body
     end
 
-    def status_code; 200; end
-
-    def body; "I am a parameter"; end
   end
 end
--- spec/lib/optimus_prime_spec.rb
+++ spec/lib/optimus_prime_spec.rb
@@ -22,4 +22,24 @@ describe OptimusPrime do
     end
   end
 
+  context "Starting and Stopping the server" do
+    it "starts the server" do
+      OptimusPrime.restart_server
+      expect( `ls ./tmp/pids` ).to include("optimus_prime.pid")
+    end
+
+    it "stops the server" do
+      OptimusPrime.start_server
+      OptimusPrime.stop_server
+      expect( `ls ./tmp/pids` ).to_not include("optimus_prime.pid")
+      OptimusPrime.start_server
+    end
+
+    it "informs me if the server is already running" do
+      OptimusPrime.start_server
+      expect( OptimusPrime.start_server ).to include("Optimus is already priming :)")
+    end
+
+  end
+
 end
